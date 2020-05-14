7079abf3163e502eaf061239313568588cd6ff55
Patrick <patrick_luiz@msn.com>
2020-05-14 09:23:20 -0300

    profile page

--- src/pages/Profile/profile-index.js
+++ src/pages/Profile/profile-index.js
@@ -1,5 +1,50 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
+
+import { updateProfileRequest } from '~/store/modules/user/actions'
+
+import { Container } from './profile-styles';
 
 export default function Profile() {
-    return <h1>Login</h1>;
+    const dispatch = useDispatch();
+    const profile = useSelector(state => state.user.profile);
+
+    function handleSubmit(data) {
+        dispatch(updateProfileRequest(data));
+    }
+
+    return (
+        <Container>
+            <Form initialData={profile} onSubmit={handleSubmit}>
+                <Input name="name" placeholder="Nome Completo" />
+                <Input
+                    name="email"
+                    type="email"
+                    placeholder="Seu Endereço de e-mail"
+                />
+
+                <hr />
+                <input
+                    type="password"
+                    name="oldPassword"
+                    placeholder="Sua senha atual"
+                />
+                <input
+                    type="password"
+                    name="password"
+                    placeholder="Nova senha"
+                />
+                <input
+                    type="password"
+                    name="confirmPassword"
+                    placeholder="Confirmação de senha"
+                />
+
+                <button type="submit">Atualizar Perfil</button>
+            </Form>
+
+            <button type="button">Sair</button>
+        </Container>
+    );
 }
